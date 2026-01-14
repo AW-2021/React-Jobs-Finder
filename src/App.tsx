6283e25cba6b751e0e3540750e8c5edc30f8045a
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2026-01-14 16:12:21 +0500

    Add SignInPage, SignUpPage, ProtectedRoute and AuthLayout to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
@@ -11,25 +12,71 @@ import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage, { companiesLoader } from "./pages/AddJobPage";
 import EditJobPage from "./pages/EditJobPage";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="jobs/:id" element={<JobPage />} loader={jobLoader} />
-      <Route
-        path="/edit-job/:id"
-        element={<EditJobPage />}
-        loader={jobLoader}
-      />
-      <Route
-        path="/add-job"
-        element={<AddJobPage />}
-        loader={companiesLoader}
-      />
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
+    <>
+      <Route path="/" element={<AuthLayout />}>
+        <Route index element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Route>
+      <Route path="/" element={<MainLayout />}>
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/jobs"
+          element={
+            <ProtectedRoute>
+              <JobsPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="jobs/:id"
+          element={
+            <ProtectedRoute>
+              <JobPage />
+            </ProtectedRoute>
+          }
+          loader={jobLoader}
+        />
+        <Route
+          path="/edit-job/:id"
+          element={
+            <ProtectedRoute>
+              <EditJobPage />
+            </ProtectedRoute>
+          }
+          loader={jobLoader}
+        />
+        <Route
+          path="/add-job"
+          element={
+            <ProtectedRoute>
+              <AddJobPage />
+            </ProtectedRoute>
+          }
+          loader={companiesLoader}
+        />
+        <Route
+          path="*"
+          element={
+            <ProtectedRoute>
+              <NotFoundPage />
+            </ProtectedRoute>
+          }
+        />
+      </Route>
+    </>
   )
 );
 
